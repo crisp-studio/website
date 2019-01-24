9a8492c8c8a57f671fd6beebd2e3bf68e752f0ec
Leo Bernard <admin@leolabs.org>
2019-01-24 15:41:14 +0100

    :sparkles: Add tests for buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,19 @@ import { Link } from 'gatsby'
 
 import Image from '../components/image'
 import Layout from '../components/layout';
+import Button from '../components/button';
+
+import icon from '../images/icons/arrow-circle-right.svg';
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
+    <Button variant="green">Hey there!</Button>
+    <Button>Yooo!</Button>
+    <Button variant="white" icon={icon}>Contact Us</Button>
   </Layout>
 )
 
