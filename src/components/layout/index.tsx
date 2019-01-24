f578723f79e393a6444f431b9f3a49f5a1c79769
Leo Bernard <admin@leolabs.org>
2019-01-24 14:10:39 +0100

    :sparkles: Include Inter UI web font

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './global-styles.css';
+import 'inter-ui/inter-ui.css';
+
+export default ({children}) => (<main>
+ {children}
+</main>)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Image from '../components/image'
+import Layout from '../components/layout';
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-  </div>
+  </Layout>
 )
 
 export default IndexPage
