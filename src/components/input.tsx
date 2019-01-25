eed1e28e4701a22b6da499e7537eb88426712fa5
Daniel Wirtz <danielwirtzx@gmail.com>
2019-01-25 17:58:01 +0100

    :lipstick: Style input component

---
+++ src/components/input.tsx
@@ -0,0 +1,21 @@
+import styled from '../util/styled-components'
+
+interface InputProps {
+  variant?: 'light' | 'dark'
+}
+
+export default styled.input<InputProps>`
+  padding: 0.5rem 1rem;
+  border-radius: ${p => p.theme.sizes.borderRadius};
+  outline: none;
+  border: none;
+  box-shadow: ${p => p.theme.shadows.inset100};
+  background-color: ${p => p.theme.colors.gray100};
+  color: ${p => p.theme.colors.gray500}
+  ${p =>
+    p.variant == 'dark' &&
+    `
+    background-color: ${p.theme.colors.gray500};
+    color: ${p.theme.colors.gray100};
+  `}
+`
