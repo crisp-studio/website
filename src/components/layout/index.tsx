ab50640204f69af8560291dd169dca9ca09c5776
Daniel Wirtz <danielwirtzx@gmail.com>
2019-01-25 18:00:39 +0100

    :lipstick: Add container wrapper

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,7 +4,16 @@ import './global-styles.css'
 import 'inter-ui/inter-ui.css'
 import { ThemeProvider } from '../../util/styled-components'
 import theme from '../../theme'
+import styled from 'styled-components';
 
 export default ({ children }) => (
-  <ThemeProvider theme={theme}><>{children}</></ThemeProvider>
+  <ThemeProvider theme={theme}><Container>{children}</Container></ThemeProvider>
 )
+
+const Container = styled.div`
+    max-width: 60rem;
+    margin: 0 auto;
+    padding: 0 2rem;
+`
+
+
