71e43b84e81f32cbfbf59d0e9438faf12d51f06b
Daniel Wirtz <danielwirtzx@gmail.com>
2019-01-25 18:00:20 +0100

    :lipstick: Update theme

--- src/theme/shadows.ts
+++ src/theme/shadows.ts
@@ -4,4 +4,6 @@ export default {
   elevation300: '0px 5px 15px rgba(0, 0, 0, 0.2)',
   elevation400: '0px 10px 24px rgba(0, 0, 0, 0.2)',
   elevation500: '0px 15px 35px rgba(0, 0, 0, 0.2)',
+  inset100:
+    'inset 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0.5px 0.5px 0px rgba(255, 255, 255, 0.15)',
 }
