2e62d3d7bf6b8ee9a5559e8df0c4891f9c4e8783
Daniel Wirtz <danielwirtzx@gmail.com>
2019-01-29 10:27:38 +0100

    :lipstick: Better styling

--- src/components/instafeed.tsx
+++ src/components/instafeed.tsx
@@ -7,6 +7,8 @@ import { render } from 'react-dom'
 import styled from '../util/styled-components'
 import breakpoint from 'styled-components-breakpoint'
 
+// import instaIcon from '../components/instagram'
+
 export default class Instagram extends React.Component {
   initialState = {
     loading: true,
@@ -40,19 +42,32 @@ export default class Instagram extends React.Component {
     if (!this.state.json) return null
 
     const { data } = this.state.json
+    console.log(data)
 
     return (
       <GridGallery>
         <Fragment>
-          {data.map(({ caption, id, images, tags }, index) => {
+          {data.map(({ images, link }, index) => {
             const image = images[options.resolution]
             return (
-              <GridItem>
-                <GridImage src={image.url} />
-                <GridHoverLink>
-                  <StyledP> CLICK ME</StyledP>
-                </GridHoverLink>
-              </GridItem>
+              <a href={link}>
+                <GridItem>
+                  <GridImage src={image.url} />
+                  <GridHoverLink>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="#ffffff"
+                        d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+                      />
+                    </svg>
+                  </GridHoverLink>
+                </GridItem>
+              </a>
             )
           })}
         </Fragment>
@@ -60,11 +75,6 @@ export default class Instagram extends React.Component {
     )
   }
 }
-
-const StyledP = styled.p`
-  color: white;
-  font-size: 2rem;
-`
 
 const GridGallery = styled.div`
   display: grid;
@@ -83,9 +93,9 @@ const GridItem = styled.div`
   display: flex;
   overflow: hidden;
   position: relative;
-  grid-column-start: auto;
-  grid-row-start: auto;
   cursor: pointer;
+
+  /* To-Do: 1:1 Ratio auf den Posts */
 
   transition: all 0.3s ease-in-out;
   box-shadow: ${p => p.theme.shadows.elevation100};
@@ -95,7 +105,6 @@ const GridItem = styled.div`
     transform: scale(1.025);
     box-shadow: ${p => p.theme.shadows.elevation300};
   }
-
 `
 
 const GridImage = styled.img`
@@ -107,23 +116,20 @@ const GridImage = styled.img`
 
 const GridHoverLink = styled.div`
   position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  transition: .5s ease;
-  /* To-Do: Switch for semi transparent color instead of making the whole thing trasparent */
-  background-color: ${p => p.theme.colors.primary400};
-
   display: flex;
   justify-content: center;
   align-items: center;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(21, 60, 255, 0);
+  z-index: 20;
+  opacity: 0;
+
+  transition: all 0.5s ease;
 
   &:hover {
-    opacity: 0.5;
+    opacity: 1;
+    background-color: rgba(21, 60, 255, 0.6);
   }
-
 `
