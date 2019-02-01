5626ec49ff6cb0bc141d222928c6a04bfcaa7333
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-01 12:26:26 +0100

    :sparkles: Add design sprint page

--- src/pages/design-sprint.tsx
+++ src/pages/design-sprint.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import Layout from '../components/modules/layout'
 import styled from '../util/styled-components'
 import SplitContent from '../components/common/splitcontent'
-import svgDesignSprint from '../images/design-sprint-illustration.svg'
 import HeroHeader from '../components/common/heroheader'
+import svgPostIts from '../images/design-sprint-post-its.svg'
+import svgExplanation from '../images/design-sprint-explanation.svg'
+import svgMonTues from '../images/design-sprint-monday-tuesday.svg'
+import svgWedThr from '../images/design-sprint-wednesday-thursday.svg'
+import svgFri from '../images/design-sprint-friday.svg'
 
 const DesignSprint = () => (
   <Layout variant="light">
@@ -23,25 +27,75 @@ const DesignSprint = () => (
           prototype tested with real users at the end of the week.{' '}
         </p>
       </div>
+      <ImageContainer>
+        <img src={svgPostIts} />
+      </ImageContainer>
+    </SplitContent>
+    <SplitContent>
+      <ImageContainer>
+        <img src={svgExplanation} />
+      </ImageContainer>
       <div>
-        <img src={svgDesignSprint} />
+        <h4>Why use a Design Sprint?</h4>
+        <p>
+          The Design Sprint boosts the speed of progress, stops guesswork and lowers
+          the risk of developing something that's not worth the effort. It helps to
+          get your product right while saving a lot of time.{' '}
+        </p>
       </div>
     </SplitContent>
     <SplitContent>
       <div>
-        <img src={svgDesignSprint} />
-      </div>
-      <div>
-        <h4>What is a Design Sprint?</h4>
+        <h4>Generate Solutions</h4>
         <p>
-          The Design Sprint is a 5-day process to create new products or improve
-          existing ones. It starts with a big challenge and ends with a high-fidelity
-          prototype tested with real users at the end of the week.{' '}
+          On Monday we'll work with you to define the challenges and the focus of the
+          week. We’ll define a long-term goal, pick a target area in the customer
+          journey and align everybody on the team. Tueday is all about having clear
+          focus and creating multiple solutions to solve the challenge.{' '}
+        </p>
+      </div>
+      <ImageContainer>
+        <img src={svgMonTues} />
+      </ImageContainer>
+    </SplitContent>
+    <SplitContent>
+      <ImageContainer>
+        <img src={svgWedThr} />
+      </ImageContainer>
+      <div>
+        <h4>Decide & Build</h4>
+        <p>
+          On Wednesday we’ll decide what solution we want to test at the end of the
+          week. We vote and create a clear sequence of screen drawings. Thursday is
+          all about bringing the drawings to life and forming a realistic, clickable
+          facade to test on Friday.{' '}
         </p>
       </div>
     </SplitContent>
+    <SplitContent>
+      <div>
+        <h4>Test & Learn</h4>
+        <p>
+          On Friday real target users will come and test the prototype on a given
+          device. The usability test is done in one-on-one interviews with the team
+          taking notes on the insights. We’ll understand why something works or
+          doesn’t work as well as where we have to readjust and iterate.{' '}
+        </p>
+      </div>
+      <ImageContainer>
+        <img src={svgFri} />
+      </ImageContainer>
+    </SplitContent>
   </Layout>
 )
+
+const ImageContainer = styled.div`
+  display: flex;
+
+  img {
+    width: 70%;
+  }
+`
 
 const StyledHeroHeader = styled(HeroHeader)`
   .children {
