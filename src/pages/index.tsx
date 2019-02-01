aef8c52bcabc586cf8d8ae9b3e6988217d74c180
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-01 12:26:45 +0100

    :sparkles: Add content to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Layout from '../components/modules/layout'
 import styled from '../util/styled-components'
 
@@ -7,27 +6,68 @@ import Button from '../components/common/button'
 import Input from '../components/common/input'
 import Instagram from '../components/modules/instagram-feed'
 import HeroHeader from '../components/common/heroheader'
+import Card, { CardHeader } from '../components/common/card'
+import targetIcon from '../images/icons/target.svg'
+import codeIcon from '../images/icons/code.svg'
+import { Section, Container } from '../components/common/section'
 
 const IndexPage = () => (
-  <Layout variant="light">
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+  <Layout variant="dark">
     <StyledHeroHeader bgColor="primary" variant="large">
       <h1>Launch better products faster.</h1>
       <p>Get your product right and move faster by testing ideas super early. </p>
     </StyledHeroHeader>
-
-    <Button variant="green">Hey there!</Button>
-
-    <br />
-    <form>
-      <Input type="text" name="test" />
-    </form>
-
-    <Instagram />
+    <StyledSection>
+      <StyledContainer>
+        <StyledCard>
+          <CardHeader icon={targetIcon}>Design Sprints</CardHeader>
+          <p>
+            Create new products or improve existing ones. Go from a big challenge to
+            realistic prototype tested by real users in 5 days.
+          </p>
+          <Button variant="green" to="/design-sprint/">
+            Design Sprint
+          </Button>
+        </StyledCard>
+        <StyledCard>
+          <CardHeader icon={codeIcon}>Dev Sprints</CardHeader>
+          <p>
+            Create new products or improve existing ones. Go from a big challenge to
+            realistic prototype tested by real users in 5 days.
+          </p>
+          <Button variant="green" to="/dev-sprint/">
+            Dev Sprint
+          </Button>
+        </StyledCard>
+      </StyledContainer>
+    </StyledSection>
+    <StyledInstagram />
   </Layout>
 )
+
+const StyledInstagram = styled(Instagram)``
+
+const StyledSection = styled(Section)`
+  margin-top: -8rem;
+  margin-bottom: 4rem;
+`
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  margin-top: -8rem;
+
+  > div:first-child {
+    margin-right: 2rem;
+  }
+`
+
+const StyledCard = styled(Card)`
+  p {
+    margin-bottom: 1rem;
+  }
+`
+
 const StyledHeroHeader = styled(HeroHeader)`
   .children {
     display: flex;
