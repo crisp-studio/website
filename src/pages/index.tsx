baf08ccf2d49c4f83ad6be8c8adaab6f7a66c0f2
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-04 15:13:23 +0100

    :sparkles: Add content to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,22 @@ import Layout from '../components/modules/layout'
 import styled from '../util/styled-components'
 
 import Button from '../components/common/button'
-import Input from '../components/common/input'
 import Instagram from '../components/modules/instagram-feed'
 import HeroHeader from '../components/common/heroheader'
 import Card, { CardHeader } from '../components/common/card'
 import targetIcon from '../images/icons/target.svg'
 import codeIcon from '../images/icons/code.svg'
 import { Section, Container } from '../components/common/section'
+import SplitContent from '../components/common/splitcontent'
+import svgUi from '../images/design-sprint-ui.svg'
+import breakpoint from 'styled-components-breakpoint'
 
 const IndexPage = () => (
   <Layout variant="dark">
-    <StyledHeroHeader bgColor="primary" variant="large">
+    <HeroHeader variant="large">
       <h1>Launch better products faster.</h1>
       <p>Get your product right and move faster by testing ideas super early. </p>
-    </StyledHeroHeader>
+    </HeroHeader>
     <StyledSection>
       <StyledContainer>
         <StyledCard>
@@ -41,11 +43,50 @@ const IndexPage = () => (
         </StyledCard>
       </StyledContainer>
     </StyledSection>
+    <SplitContent>
+      <ImageContainer>
+        <img src={svgUi} />
+      </ImageContainer>
+      <div>
+        <h4>Who we are</h4>
+        <p>
+          We’re a Digital Product Studio helping product teams to get their products
+          right. It’s our job to compress the pre-launch process, test new ideas and
+          validate existing ones. With the aim to create engaging products which
+          delight both you and your users.
+        </p>
+      </div>
+    </SplitContent>
+    <Section>
+      <Container>
+        <InstagramIntro>
+          <h4>Our Journey</h4>
+          <p>
+            Enjoy the backstage view and learn how we work. We’re driven to share
+            everything we learn with you.
+          </p>
+        </InstagramIntro>
+      </Container>
+    </Section>
     <StyledInstagram />
   </Layout>
 )
 
-const StyledInstagram = styled(Instagram)``
+const InstagramIntro = styled.div`
+  padding: 2rem 0;
+`
+
+const ImageContainer = styled.div`
+  display: flex;
+
+  img {
+    width: 70%;
+  }
+`
+
+const StyledInstagram = styled(Instagram)`
+  padding: 4rem;
+`
 
 const StyledSection = styled(Section)`
   margin-top: -8rem;
@@ -57,34 +98,23 @@ const StyledContainer = styled(Container)`
   justify-content: space-between;
   margin-top: -8rem;
 
+  ${breakpoint('mobile', 'tablet')`
+    flex-wrap: wrap;
+  `}
+
   > div:first-child {
     margin-right: 2rem;
+
+    ${breakpoint('mobile', 'tablet')`
+    margin-right: 0;
+    margin-bottom: 1rem;
+  `}
   }
 `
 
 const StyledCard = styled(Card)`
   p {
     margin-bottom: 1rem;
-  }
-`
-
-const StyledHeroHeader = styled(HeroHeader)`
-  .children {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    text-align: center;
-    h1 {
-      color: ${p => p.theme.colors.supWhite};
-      margin-bottom: 1rem;
-    }
-    p {
-      font-size: 1.5rem;
-      max-width: 30rem;
-      color: ${p => p.theme.colors.gray100};
-    }
   }
 `
 
