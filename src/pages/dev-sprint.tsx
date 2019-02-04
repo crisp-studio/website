f28508244be0cfd0fc69ccafd19d59948f04cdfb
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-04 15:13:08 +0100

    :sparkles: Add dev sprint page

--- src/pages/dev-sprint.tsx
+++ src/pages/dev-sprint.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Layout from '../components/modules/layout'
+import HeroHeader from '../components/common/heroheader'
 
 const Devsprint = () => (
   <Layout variant="light">
-    <h1>Dev Sprint!</h1>
+    <HeroHeader variant="small">
+      <h1>
+        Dev <br />
+        Sprint
+      </h1>
+      <p>
+        Solve big product challenges and test new ideas in 5 days. Get user insights
+        before launching and see how customers react.{' '}
+      </p>
+    </HeroHeader>
   </Layout>
 )
 
