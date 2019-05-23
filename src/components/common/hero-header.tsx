038f233aef1bdd4f477a6ae2ec6c4733dda3ba4a
Leo Bernard <admin@leolabs.org>
2019-05-23 17:17:30 +0200

    :lipstick: Fix small blue line in some browsers on retina

--- src/components/common/hero-header.tsx
+++ src/components/common/hero-header.tsx
@@ -102,7 +102,7 @@ export default styled(HeroHeader)<HeroHeaderProps>`
   &:after {
     content: '';
     position: absolute;
-    bottom: 0;
+    bottom: -1px;
     width: 100%;
     height: 4vw;
     background-position: bottom;
