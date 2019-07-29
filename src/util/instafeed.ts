5733a7b3d92264d042f9b313e4d716a3606880c3
Leo Bernard <admin@leolabs.org>
2019-07-29 16:52:31 +0200

    :lock: Move Instagram config into env variables

--- src/util/instafeed.ts
+++ src/util/instafeed.ts
@@ -2,8 +2,8 @@ import { buildUrl } from 'react-instafeed'
 import instafeed from 'react-instafeed'
 
 export const options = {
-  accessToken: '3090055275.1677ed0.7323fcce6c7b4d19be8e4b0302084eb4',
-  clientId: '5e81bc35f1b444588d55feaad21783d3',
+  accessToken: process.env.GATSBY_IG_TOKEN,
+  clientId: process.env.GATSBY_IG_ID,
   get: 'user', // popular, user
   locationId: null,
   resolution: 'standard_resolution', // thumbnail, low_resolution, standard_resolution
